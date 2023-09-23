aba6629c6f83e290e8fe02020c8034e2414531d5
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-23 23:10:31 +0600

    add json file

---
+++ src/Components/JobCategory/JobCategory.jsx
@@ -0,0 +1,12 @@
+
+const JobCategory = () => {
+    return (
+        <div className="text-center">
+            
+            <h2 className="text-5xl font-extrabold mt-20">Job Category List</h2>
+            <p className="text-base font-medium my-5">Explore thousands of job opportunities with all the information you need. Its your future</p>
+        </div>
+    );
+};
+
+export default JobCategory;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../Components/Heade/Header";
+import JobCategory from "../../Components/JobCategory/JobCategory";
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <JobCategory></JobCategory>
         </div>
     );
 };
